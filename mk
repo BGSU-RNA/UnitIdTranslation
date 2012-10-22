7860f78fb76a5d62bee92640d2238745b3948559
Blake Sweeney <blakes.85@gmail.com>
2012-10-22 13:31:01 -0400

    Package into a python package.
    This can now be installed and used as a standard python package. It does assume
    that CorePyWrap, NDB's cif parser package, is installed. It doesn't list
    CorePyWrap as a dependency because the wrapper has to be manually installed.

--- mk
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-[[ -e CorePyWrap.so ]] || lk CorePyWrap.so ~/ndb/cif-wrapper/wrapper/CorePyWrap.so
-[[ -e bin/CorePyWrap.so ]] || lk bin/CorePyWrap.so ~/ndb/cif-wrapper/wrapper/CorePyWrap.so
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "id_translate",
+    version = "0.1.0",
+    author = "Blake Sweeney",
+    author_email = "bsweeney@bgsu.edu",
+    description = ("Translate old to new style ids"),
+    license = "BSD",
+    py_modules = ["id_translate"],
+)

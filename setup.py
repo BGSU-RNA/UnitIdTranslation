b2ce0a2d1ffb4bbc790940bc431e859d556361e4
Blake Sweeney <blakes.85@gmail.com>
2012-10-23 16:13:32 -0400

    Version dump and declare dependencies

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name = "id_translate",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Blake Sweeney",
     author_email = "bsweeney@bgsu.edu",
     description = ("Translate old to new style ids"),
     license = "BSD",
     py_modules = ["id_translate"],
+    requires = ['biopython>=1.60'],
 )

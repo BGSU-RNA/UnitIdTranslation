4912490c92ea61cba88afa7a9ae6fe0b6dcbd5b3
Blake Sweeney <blakes.85@gmail.com>
2012-11-07 13:44:29 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "id_translate",
-    version = "0.1.4",
+    version = "0.1.5",
     author = "Blake Sweeney",
     author_email = "bsweeney@bgsu.edu",
     description = ("Translate old to new style ids"),
